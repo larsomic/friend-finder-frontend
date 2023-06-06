4eacc3169da456173c2c7b58fe20fb7655efb932
larsomic <michael.j.larson@wsu.edu>
2023-06-05 21:11:30 -0700

    fix merge conflicts

--- redux/store_type.tsx
+++ redux/store_type.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export interface LoginActions {
     type: 'LOG_IN' | 'LOG_OUT' | 'SIGNED_UP';
   }
@@ -10,12 +9,3 @@ interface AuthState {
 export interface StoreType {
     auth: AuthState;
 }
-=======
-export interface StoreType {
-    loggedIn: boolean;
-}
-
-export interface LoginActions {
-    type: 'LOG_IN' | 'LOG_OUT';
-  }
->>>>>>> a860dfd059d8dd22f2a535227fa29745b01a95bf
