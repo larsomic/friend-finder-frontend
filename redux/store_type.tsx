d259e2a0db07e4723077d1669a629595a43ccfdd
larsomic <michael.j.larson@wsu.edu>
2023-06-17 20:05:40 -0700

    Add edit user frontend

--- redux/store_type.tsx
+++ redux/store_type.tsx
@@ -6,6 +6,22 @@ interface AuthState {
     loggedIn: boolean;
 }
 
+export interface UserActions {
+    type: 'GET_USER_INFO' | 'UPDATE_USER_INFO';
+  }
+
+interface UserState {
+    name: string;
+    email: string;
+}
+
 export interface StoreType {
     auth: AuthState;
+    user: UserState;
 }
+
+
+export const updateUserInfo = (name: string, email: string): UserActions => ({
+    type: 'UPDATE_USER_INFO',
+    payload: { name, email },
+  });
